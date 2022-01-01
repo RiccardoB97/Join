e2334c5a9c0cca9d2497b5a1835c0a77a4cf65eb
RiccardoB97 <beraldo.riccardo@gmail.com>
2022-01-01 17:51:38 +0000

    Implement authentication when logged in

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -6,8 +6,14 @@ use Illuminate\Http\Request;
 
 class AdminController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware('role:superadministrator');
+    }
+
     public function index()
     {
         return view('admin.index');
     }
+
 }
